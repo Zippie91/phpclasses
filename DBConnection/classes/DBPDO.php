c2f0c52b34ed14ae41a0b506b78e46f8e0fe7dfa
Raf Smits <raf_smits@hotmail.com>
2016-10-03 16:29:00 +0200

    Inital init

---
+++ DBConnection/classes/DBPDO.php
@@ -0,0 +1,28 @@
+<?php
+class DBConnection extends PDO
+{
+    private $db;
+    private $error;
+	
+    public function __construct($file = "../config/config.ini") 
+	{
+        if (!$config = parse_ini_file($file, true)) throw new exception('Unable to open ' . $file . '.');
+		
+        $dsn = "{$config['engine']}:host={$config['host']};dbname={$config['name']}";
+        $options = array(
+            PDO:ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+		
+        try {
+		    $this->db = new PDO($dsn, $config['user'], $config['password'], $options);
+        } catch (PDOException $e) {
+            $this->error = $e->getMessage();
+        }
+    }
+	
+    public function closeConnection() {
+        $this->db = null;
+    }
+}
+?>
---
+++ DBConnection/config/config.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_NAME", "dbname");
+define("DB_USER", "user");
+define("DB_PW", "password");
+define("DB_ENGINE", "mysql");
+?>
